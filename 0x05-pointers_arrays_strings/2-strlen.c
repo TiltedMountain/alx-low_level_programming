f6fc9fc93c8522fea0b632b42dfbe844ceab689d
Stephen <stephenwward001@gmail.com>
2023-02-27 01:39:45 -0800

    2-fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,13 @@
  *
  * Return: The length of @str.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
-
-	while (*str++)
-		length++;
-
-	return (length);
+	int longi = 0;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return(longi);
 }
